4d275bf88863eff54d413cf5370ac252a5e5ac18
Samuelcapucho <128175305+Samuelcapucho@users.noreply.github.com>
2023-06-30 13:13:21 -0300

    Add files via upload

--- exemploWith.py
+++ exemploWith.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[27]:
+# In[12]:
 
 
 import streamlit as st
@@ -25,11 +19,11 @@ if st.session_state["arquivoObject"] != None:
     st.session_state["arquivoObject"] = st.session_state["arquivoObject"].getvalue()
 
     
-with ZipFile('intermedio.zip', 'w') as myzip:
+with ZipFile('https://github.com/Samuelcapucho/testFiles/blob/main/intermedio.zip', 'w') as myzip:
     myzip.writestr('protocolo.pdf', st.session_state["arquivoObject"])
 
 
-with ZipFile('intermedio.zip', 'r') as myzipTwo:
+with ZipFile('https://github.com/Samuelcapucho/testFiles/blob/main/intermedio.zip', 'r') as myzipTwo:
     bytesData = myzipTwo.read()
 
 st.write(bytesData)
